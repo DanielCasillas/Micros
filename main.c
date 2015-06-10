9022ce5b18f29dda529675c0b09f9a7739cab37b
DanielCasillas <daniel.casillas@edu.uag.mx>
2015-06-10 15:56:38 -0500

    LAB01

---
+++ main.c
@@ -0,0 +1,9 @@
+//HW LAB01
+//functionsfor27/05/2015
+int calculateSquareArea (int iSide);
+int calculateCirclePerimeter(int iRadius);
+Bool isEven(int iNumber);
+Bool isPrime(int number);
+void showFibonacci(void);
+
+
